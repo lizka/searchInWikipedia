2f98b763e8d0a3b9a05b0ab59e046aa2d9c68ece
lizka <lizkaaa@icloud.com>
2017-10-09 18:46:04 +0200

    Limit results, fix callback

--- src/app/wiki.service.ts
+++ src/app/wiki.service.ts
@@ -10,7 +10,7 @@ export class WikiService {
   constructor(private jsonp: Jsonp) { }
 
   search(query: string) {
-    const apiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&generator=links&list=search&srsearch=${query}&utf8=1`;
+    const apiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&generator=links&list=search&srsearch=${query}&utf8=1&limit=20&callback=JSONP_CALLBACK`;
 
     return this.jsonp.request(apiUrl)
         .map(res => {
