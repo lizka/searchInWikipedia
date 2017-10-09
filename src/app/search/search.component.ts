34ece2d8e68d7ed576be02889c78d3aa3e220577
lizka <lizkaaa@icloud.com>
2017-10-09 19:42:22 +0200

    Add error handling and empty state

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -11,15 +11,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  private loading: boolean = false;
-  private results: Observable<SearchItem[]>;
+  private results$: Observable<SearchItem[]>;
+  private loading: Boolean = false;
   private searchInput: FormControl;
 
   constructor(private wikiService: WikiService) { }
 
   ngOnInit() {
     this.searchInput = new FormControl();
-    this.results = this.searchInput.valueChanges
+    this.results$ = this.searchInput.valueChanges
         .debounceTime(100)
         .distinctUntilChanged()
         .do(_ => this.loading = true)
--- src/app/wiki.service.ts
+++ src/app/wiki.service.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs/Rx';
 import { SearchItem } from './SearchItem';
 import { Injectable } from '@angular/core';
 import { Jsonp, Http } from '@angular/http';
@@ -10,16 +11,23 @@ export class WikiService {
   constructor(private jsonp: Jsonp) { }
 
   search(query: string) {
-    const apiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&generator=links&list=search&srsearch=${query}&utf8=1&limit=20&callback=JSONP_CALLBACK`;
+    try {
+      const apiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&generator=links&list=search&srsearch=${query}&utf8=1&limit=20&callback=JSONP_CALLBACK`;
 
-    return this.jsonp.request(apiUrl)
+      return this.jsonp.request(apiUrl)
         .map(res => {
-          return res.json().query.search.map(item => {
-            return new SearchItem(
-              item.title,
-              item.wordcount
-            );
-          });
+          if (res.json().query) {
+            return res.json().query.search.map(item => {
+              return new SearchItem(
+                item.title,
+                item.wordcount
+              );
+            });
+          }
         });
+    } catch (error) {
+      return Observable.empty<Response>();
+    }
+
   }
 }
